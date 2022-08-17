ef164235e4b23f7ca14732ea5218bed0aad4b238
wzt <wzt@163.com>
2022-08-17 14:36:53 +0800

    master commit3

--- src/main/java/com/wzt/git/GitTest.java
+++ src/main/java/com/wzt/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("Hello Git IDEA!");
         System.out.println("Hello World!");
         System.out.println("Hot Fix!");
+        System.out.println("Master!");
     }
 }
