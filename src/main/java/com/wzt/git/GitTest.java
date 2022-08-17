eaef61f6ead1709ee73f25159921cc76114988c9
wzt <wzt@163.com>
2022-08-17 14:26:58 +0800

    third commit

--- src/main/java/com/wzt/git/GitTest.java
+++ src/main/java/com/wzt/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git IDEA!");
         System.out.println("Hello World!");
+        System.out.println("Hot Fix!");
     }
 }
